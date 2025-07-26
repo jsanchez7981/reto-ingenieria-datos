e1cecccd914b6cc02a95521be55ea1a3aaa11e57
jsanchez7981 <58012762+jsanchez7981@users.noreply.github.com>
2025-07-26 13:44:16 -0500

    Update db_utils.py
    Cambio de contraseña

--- pipeline/db_utils.py
+++ pipeline/db_utils.py
@@ -1,4 +1,4 @@
 from sqlalchemy import create_engine
 
 def get_engine():
-    return create_engine('postgresql://isabella:Jupiter164*@localhost:5432/pragma')
+    return create_engine('postgresql://isabella:contraseña@localhost:5432/pragma')
